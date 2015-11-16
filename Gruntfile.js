57d419a9c15b777b90db7e27de5e13ea77b0f934
Fred Maya <archonsyndicate@gmail.com>
2015-11-15 18:27:37 -0800

    basic Node env. and build process

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+/**
+ * General Grunt setup
+ */
+'use strict';
+
+var path = require('path');
+
+module.exports = function (grunt) {
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+  // Time how long tasks take (to optimize build times)
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    dirs: {
+      output: 'build'
+    },
+
+    connect: {
+        server: {
+          options: {
+            port: 9001,
+            livereload: true,
+            base: 'app'
+          }
+        }
+    },
+
+    watch: {
+      scss: {
+        files: 'app/scss/**/*.scss',
+        tasks: ['sass:dev']
+      },
+      reload: {
+        files: ['app/**/*.css', 'app/**/*.html'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'app/css/main.css': 'app/scss/main.scss'
+        }
+      },
+      dist: {
+        options: {
+          outputStyle: "compressed"
+        },
+        files: {
+          'app/css/main.css': 'app/scss/main.scss'
+        }
+      }
+    },
+
+    // Minify html
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'app/index.min.html': 'app/index.html'     // 'destination': 'source'
+        }
+      }
+    },
+
+    // The banner
+    tag: {
+      banner: '/*!\n' +
+      ' * <%= pkg.name %>\n' +
+      ' * <%= pkg.description %>\n' +
+      ' * @author <%= pkg.author.name %>\n' +
+      ' * @version <%= pkg.version %>\n' +
+      ' */\n'
+    }
+
+  });
+
+  grunt.registerTask('default', [
+    'sass:dev',
+    'connect',
+    'watch'
+    ]);
+
+  grunt.registerTask('build', [
+    'sass:dist',
+    'htmlmin'
+    ]);
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+## README
+----------
+A little experimental UI module built using NodeJS, GruntJS and Node-Sass
+
+### Install dependencies:
+`$ npm Install && bower install girder`
+
+### Run tasks for Dev
+`$ grunt`
