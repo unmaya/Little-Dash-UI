3162e2d4cb159eab7cb3ef5269c899e7e2708fad
Fred Maya <archonsyndicate@gmail.com>
2015-11-16 20:53:39 -0800

    add css and js output

---
+++ app/dist/js/main.js
@@ -0,0 +1,7 @@
+/*!
+ * LittleDashUI
+ * A little UI / UX experiment.
+ * @author Fred Maya
+ * @version 0.1.1
+ */
+$(document).ready(function(){$(".spendings").click(function(a){$(".drawer").toggle(".is-active"),a.preventDefault()})});
