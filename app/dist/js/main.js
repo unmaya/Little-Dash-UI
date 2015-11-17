04560edcd5e9945437a1c3a41cb1edfea5c68416
Fred Maya <archonsyndicate@gmail.com>
2015-11-17 01:08:12 -0800

    add a notifications dropdown

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -4,4 +4,4 @@
  * @author Fred Maya
  * @version 0.1.1
  */
-$(document).ready(function(){$(".spendings").click(function(a){$(".drawer").toggle(".is-active"),a.preventDefault()})});
+$(document).ready(function(){$(".btn.add-money, .drawer-close").click(function(a){$(".drawer").toggle(300),a.preventDefault()}),$(".header-notify .notifications").click(function(a){$(".notifications-view").toggleClass("is-active animated"),a.preventDefault()})});
